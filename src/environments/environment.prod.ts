e4832d6e20c7121a68ab0fb8e510f920cb756c2b
turboaux <jje.uribe@gmail.com>
2023-06-17 21:43:47 -0600

    Agrega la configuración de contentful a los entornos de prueba y producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-    production: true
+    production: true,
+    contentful: {
+        spaceId: 'jc7x442ybiq3', 
+        accessToken: 'ce_RFwwWfUZppVSsvSZHl7tCniMl3hG1x0Vqu8YItyg',
+        environment: 'master',
+        contentTypeIds: {
+            users: 'user', 
+            calorieMetrics: 'calorieMetric', 
+            products: 'product'
+        }
+    },
 };
