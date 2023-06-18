ad220f0addbb49de34f80c114c304b1e62ff8f83
turboaux <jje.uribe@gmail.com>
2023-06-18 16:22:26 -0600

    Establece el componente product-card para que siga una estrategia de deteccion de cambios OnPush

--- src/app/ui/product-card/components/product-card/product-card.component.ts
+++ src/app/ui/product-card/components/product-card/product-card.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'], 
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductCardComponent {
 
